069ef771d758c931abb96b9391f6f8c1410c6122
54V4NT <54V4NT@users.noreply.github.com>
2018-07-25 17:46:31 -0600

    Initial wallet config
    We moved slowly. But still mistakes were probably made.

--- src/zedwallet/WalletConfig.h
+++ src/zedwallet/WalletConfig.h
@@ -10,10 +10,10 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string addressPrefix = "TRTL";
+    const std::string addressPrefix = "SETH";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
-    const std::string ticker = "TRTL";
+    const std::string ticker = "SETH";
 
     /* The filename to output the CSV to in save_csv */
     const std::string csvFilename = "transactions.csv";
@@ -23,10 +23,10 @@ namespace WalletConfig
     const std::string addressBookFilename = ".addressBook.json";
 
     /* The name of your deamon */
-    const std::string daemonName = "TurtleCoind";
+    const std::string daemonName = "CanaryCoind";
 
     /* The name to call this wallet */
-    const std::string walletName = "zedwallet";
+    const std::string walletName = "SethWallet";
 
     /* The name of walletd, the programmatic rpc interface to a wallet */
     const std::string walletdName = "service";
@@ -35,7 +35,7 @@ namespace WalletConfig
     const std::string coinName = std::string(CryptoNote::CRYPTONOTE_NAME);
 
     /* Where can your users contact you for support? E.g. discord */
-    const std::string contactLink = "http://chat.turtlecoin.lol";
+    const std::string contactLink = "http://canary.futurepow.com";
 
 
     /* The number of decimals your coin has */
